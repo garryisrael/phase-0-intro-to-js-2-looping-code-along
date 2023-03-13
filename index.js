5c993ad8b9d868ba09a1bf9435c75ad3151ae4e1
Garry <garryisrael02@gmail.com>
2023-03-13 19:03:44 -0400

    learns looping

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+ 
+  function writeCards(guests, event) {
+    let thankYouCards = []
+    for (let i = 0; i < guests.length; i++ ) {
+      thankYouCards.push( `Thank you, ${guests[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+function countDown(num){
+  while(num>=0){
+    console.log(num);
+    num--;
+}
+}
